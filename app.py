2d413ccd3ed51543525a68d82d61116be035aa7d
Andres <qui18288@uvg.edu.gt>
2022-10-17 19:40:36 -0600

    main done

--- app.py
+++ app.py
@@ -170,3 +170,231 @@ tetragrama = ngram(tetragrama, size)
 pentgrama=[]
 size=5
 pentgrama = ngram(pentgrama, size)
+
+#MODELO 1: PENTAGRAMA
+model = defaultdict(lambda: defaultdict(lambda: 0))
+for i,j,k,l,m in pentgrama:
+    model[(i,j)][k,l,m] += 1
+for i,j in model:
+    total=float(sum(model[(i,j)].values()))
+    for k,l,m in model[(i,j)]:
+        model[(i,j)][k,l,m] /= total
+
+#MODELO 2: DIGRAMA
+model2 = defaultdict(lambda: defaultdict(lambda: 0))
+for i,j in digrama:
+    model2[(i)][j] += 1
+for i in model2:
+    total=float(sum(model2[(i)].values()))
+    for j in model2[(i)]:
+        model2[(i)][j] /= total
+
+
+#MODELO 3: TRIAGRAMA
+model3 = defaultdict(lambda: defaultdict(lambda: 0))
+for i,j,k in trigrama:
+    model3[(i)][j,k] += 1
+for i in model3:
+    total=float(sum(model3[(i)].values()))
+    for j,k in model3[(i)]:
+        model3[(i)][j,k] /= total
+
+#MODELO 4: TETRAGRAMA
+model4 = defaultdict(lambda: defaultdict(lambda: 0))
+for i,j,k,l in tetragrama:
+    model4[(i,j)][k,l] += 1
+for i,j in model4:
+    total=float(sum(model4[(i,j)].values()))
+    for k,l in model4[(i,j)]:
+        model4[(i,j)][k,l] /= total
+
+#MODELO DE PREDICCION 1
+def prediction(input1,input2):
+    predictions = {}
+    for word1, word2, word3 in model[(input1,input2)].keys():
+        predictions[word1] = 1 
+        predictions[word2] = 1 
+        predictions[word3] = 1
+    for word1, word2 in model4[(input1,input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+        predictions[word3] = predictions[word3] + 1 if word3 in predictions else 1
+    for word1, word2 in model3[(input1)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+    for word1, word2 in model3[(input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+    for word1 in model2[(input1)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+    for word1 in model2[(input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+    return predictions
+
+#MODELO DE PREDICCION 2
+def prediction2(input1,input2):
+    predictions = {}
+    for word1, word2, word3 in model[(input1,input2)].keys():
+        predictions[word1] = 1 
+        predictions[word2] = 1 
+        predictions[word3] = 1
+    for word1, word2 in model4[(input1,input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+        predictions[word3] = predictions[word3] + 1 if word3 in predictions else 1
+    for word1, word2 in model3[(input1)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+    return predictions
+
+#MODELO DE PREDICCION 3
+def prediction3(input1,input2):
+    predictions = {}
+    for word1, word2, word3 in model[(input1,input2)].keys():
+        predictions[word1] = 1 
+        predictions[word2] = 1 
+        predictions[word3] = 1
+    for word1, word2 in model3[(input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+        predictions[word2] = predictions[word2] + 1 if word2 in predictions else 1
+    for word1 in model2[(input1)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+    for word1 in model2[(input2)].keys():
+        predictions[word1] = predictions[word1] + 1 if word1 in predictions else 1
+    return predictions
+    
+
+# App Layout
+app.layout = html.Div([
+
+    html.H1("Tweets, blogs y noticias en inglés", style={'text-align': 'center', 'family':'Verdana'}),
+    html.H3("Link: https://www.kaggle.com/crmercado/tweets-blogs-news-swiftkey-dataset-4million/", style={'text-align': 'center'}),
+    html.H3("El data set utilizado es una recopilación de tweets, blogs y noticias en inglés donde se hallaron las palabras mas utilizadas. Lo que se pretende hacer es poder hacer un análisis sobre los textos para poder determinar qué tipos de palabras son las que tienen mayor frecuencia en cada uno de los archivos, y determinar qué tanta presencia tienen estas", style={'text-align': 'center'}),
+    html.Button("NAVEGAR AL SIGUIENTE",id="next",n_clicks=0, style={'text-align': 'center', 'align-items':'center','justify-content':'center',  'height': 50, 'width': 150, 'color': 'white', 'background': '#004AAD'}),
+    html.H3(random.choice(normalized[0] + normalized[1] + normalized[2]),id="data",style={'background':'#55ADEE','min-height':300, 'display': 'flex', 'align-items':'center','justify-content':'center', 'padding': 16, 'text-align': 'center','border-radius': '10px', 'color': 'white', 'margin-right': 350, 'margin-left': 350, 'align-self': 'center'}),
+
+    html.H2("Modelos de predicción",style={'text-align': 'center'}),
+
+    dcc.Input(
+        id="model",
+        type="text",
+        style={'align-self': 'center','width': 500, 'height': 50, 'margin-left': 50,},
+        placeholder="Escribe las palabras a predecir... ",
+    ),
+
+    html.H5(id="model_catch",style={'color': 'red', 'margin-left': 50,}),
+
+    dcc.Graph(id="model1"),
+    dcc.Graph(id="model2"),
+    dcc.Graph(id="model3"),
+
+    dcc.Dropdown(
+        id='freq-dropdown',
+        options=[
+            {'label': 'Tweets', 'value': 'tweets'},
+            {'label': 'Blogs', 'value': 'blogs'},
+            {'label': 'Noticias', 'value': 'news'}
+        ],
+        value='tweets'
+    ),
+    dcc.Graph(id="freq-hist"),
+
+    html.Br(),
+
+])
+
+@app.callback(
+    Output("data", "children"),
+    Input('next', 'n_clicks'),
+)
+def update_result(n_clicks):
+    return random.choice(normalized[0] + normalized[1] + normalized[2])
+
+@app.callback(
+    Output("model_catch", "children"),
+    Input("model", "value"),
+)
+def update_result(model):
+    words = str(model).split(" ")
+    if (len(words)!=2):
+        return "Ingrese exactamente dos palabras."
+    else:
+        return ""
+
+@app.callback(
+    Output("model1", "figure"),
+    [Input("model", "value")],
+)
+def update_result(model):
+    words = str(model).split(" ")
+    if (len(words)==2):
+        predictions_1 = prediction(words[0],words[1])
+        my_predictions1 = dict(sorted(predictions_1.items(), key=lambda item: item[1], reverse=True))
+        prediction_ = pd.DataFrame(list(zip(my_predictions1.keys(), [item/10 for item in my_predictions1.values()])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo 1 predicción")
+        return fig
+    else:
+        prediction_ = pd.DataFrame(list(zip([], [])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo 1 predicción")
+        return fig
+
+@app.callback(
+    Output("model2", "figure"),
+    [Input("model", "value")],
+)
+def update_result(model):
+    words = str(model).split(" ")
+    if (len(words)==2):
+        predictions_1 = prediction2(words[0],words[1])
+        my_predictions1 = dict(sorted(predictions_1.items(), key=lambda item: item[1], reverse=True))
+        prediction_ = pd.DataFrame(list(zip(my_predictions1.keys(), [item/10 for item in my_predictions1.values()])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo 2 predicción")
+        return fig
+    else:
+        prediction_ = pd.DataFrame(list(zip([], [])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo 2 predicción")
+        return fig
+
+@app.callback(
+    Output("model3", "figure"),
+    [Input("model", "value")],
+)
+def update_result(model):
+    words = str(model).split(" ")
+    if (len(words)==2):
+        predictions_1 = prediction3(words[0],words[1])
+        my_predictions1 = dict(sorted(predictions_1.items(), key=lambda item: item[1], reverse=True))
+        prediction_ = pd.DataFrame(list(zip(my_predictions1.keys(), [item/10 for item in my_predictions1.values()])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo 3 predicción")
+        return fig
+    else:
+        prediction_ = pd.DataFrame(list(zip([], [])),columns =['Word', 'Probabilty'])
+        fig = px.bar(prediction_, x="Word", y="Probabilty", title="Modelo  predicción")
+        return fig
+
+@app.callback(
+    Output("freq-hist", "figure"), 
+    Input("freq-dropdown", "value"))
+def display_color(value):
+    if(value == "tweets"):
+        fig = px.histogram(tweet_frequency[:5], x="Word", y="Frequency", title="Palabras más frecuentes en tweets")
+        return fig
+    elif(value == "blogs"):
+        fig = px.histogram(blogs_frequency[:5], x="Word", y="Frequency", title="Palabras más frecuentes en Blogs")
+        return fig
+    elif(value == "news"):
+        fig = px.histogram(news_frequency[:5], x="Word", y="Frequency", title="Palabras más frecuentes en noticias")
+        return fig
+    else:
+        freq = pd.DataFrame(list(zip([], [])),columns =['Word', 'Frequency'])
+        fig = px.bar(freq, x="Word", y="Frequency", title="Seleccione un medio")
+        return fig
+        
+    
+    
+    
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
